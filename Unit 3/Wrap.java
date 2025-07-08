42f52767fe607e5abe77ed7a07a4eb7fa953dc36
Gaurav1838 <109055217+Gaurav1838@users.noreply.github.com>
2025-07-08 09:49:22 +0530

    Create Wrap.java

---
+++ Unit 3/Wrap.java
@@ -0,0 +1,13 @@
+class Wrap
+{
+	public static void main(String args[])
+	{
+		// Integer iob = new Integer(100); //boxing
+		// int i = iob.intValue();		//unboxing
+		// System.out.println(i + " " + iob);
+
+		Integer iob = 100; //auto boxing
+		int i = iob; //auto unboxing
+		System.out.println(i + " " + iob);
+	}
+}
