afbec7e54a2be569ba065cbc406c7bcc9aa642ad
Gaurav1838 <109055217+Gaurav1838@users.noreply.github.com>
2025-07-22 10:05:38 +0530

    Create LambdaFactorial.java

---
+++ Unit 4/LambdaFactorial.java
@@ -0,0 +1,23 @@
+interface NumericFunc
+{
+	int func(int n);
+}
+
+class LambdaFactorial 
+{
+	public static void main(String args[])
+	{
+		
+		NumericFunc factorial = (n) ->{
+			int res = 1;
+			for(int i =1; i<=n; i++)
+			{
+				res = res*i;
+			}
+			return res;
+		};
+		System.out.println("The factorial is "+factorial.func(5));
+		System.out.println("The factorial is "+factorial.func(6));
+			
+	}
+}
